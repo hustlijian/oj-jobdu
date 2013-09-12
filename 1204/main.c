93eac97c601649dde78e3c5711a852a986e1e11b
lijian <hustlijian@hust.edu.cn>
2013-09-12 17:57:24 +0800

    add 1204

---
+++ 1204/main.c
@@ -0,0 +1,27 @@
+/*使用分步图，罗列情况就可以出来了
+ * */
+#include<stdio.h>
+
+int main()
+{
+        printf("sheep_go\n");
+        printf("nothing_come\n");
+        printf("vegetable_go\n");
+        printf("sheep_come\n");
+        printf("wolf_go\n");
+        printf("nothing_come\n");
+        printf("sheep_go\n");
+        printf("succeed\n");
+        printf("\n");
+
+        printf("sheep_go\n");
+        printf("nothing_come\n");
+        printf("wolf_go\n");
+        printf("sheep_come\n");
+        printf("vegetable_go\n");
+        printf("nothing_come\n");
+        printf("sheep_go\n");
+        printf("succeed\n");
+        printf("\n");
+        return(0);
+}

0aa951bc097d82e6f06eee1be7c5d2b396698c5a
lijian <hustlijian@hust.edu.cn>
2013-11-14 15:59:35 +0800

    better way for 1074

--- 1074/main.c
+++ 1074/main.c
@@ -3,21 +3,18 @@
 #include <time.h>
 
 char a[10];
-char b[10];
 int checked(int n)
 {
     int i, j, l;
-    char t;
     sprintf(a, "%d", n);
     //printf("a: %s, len:%d\n", a, strlen(a));
     l = strlen(a);
-    strcpy(b, a);
     for (i = 0, j=l-1;i<j ; i++,j--) {
-        t = a[i],a[i] = a[j],a[j] = t;
+        if (a[i]!=a[j]) {
+            return 0;
+        }
     }
-    b[l] = '\0';
-    //printf("b: %s\n", b);
-    return !strcmp(a,b);
+    return 1;
 }
 int main(int argc, char* argv[])
 {
